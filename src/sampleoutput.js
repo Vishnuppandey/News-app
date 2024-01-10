995fc02b0a85b38485068f8580cb56e5a143e62c
Vishnuppandey <pandeyvishnukant82@gmail.com>
2024-01-10 22:28:11 +0530

    without api key

---
+++ src/sampleoutput.js
@@ -0,0 +1,58 @@
+{
+  "status": "ok",
+  "totalResults": 4,
+  "articles": [
+    {
+      "source": {
+        "id": "google-news-in",
+        "name": "Google News (India)"
+      },
+      "author": "NDTV",
+      "title": "Cricketer To Conman: How A 25-Year-Old Cheated Rishabh Pant Of Rs 1.6 Crore - NDTV",
+      "description": null,
+      "url": "https://news.google.com/rss/articles/CBMipQFodHRwczovL3d3dy5uZHR2LmNvbS9pbmRpYS1uZXdzL2NyaWNrZXRlci10by1jb25tYW4taG93LWEtMjUteWVhci1vbGQtY2hlYXRlZC1pbmRpYS1jcmlja2V0ZXItcmlzaGFiaC1wYW50LW9mLXJzLTEtNi1jcm9yZS1tcmluYW5rLXNpbmdoLWNvbm1hbi1tcmluYW5rLXNpbmdoLTQ3NTU3NTPSAasBaHR0cHM6Ly93d3cubmR0di5jb20vaW5kaWEtbmV3cy9jcmlja2V0ZXItdG8tY29ubWFuLWhvdy1hLTI1LXllYXItb2xkLWNoZWF0ZWQtaW5kaWEtY3JpY2tldGVyLXJpc2hhYmgtcGFudC1vZi1ycy0xLTYtY3JvcmUtbXJpbmFuay1zaW5naC1jb25tYW4tbXJpbmFuay1zaW5naC00NzU1NzUzL2FtcC8x?oc=5",
+      "urlToImage": null,
+      "publishedAt": "2023-12-28T04:18:58+00:00",
+      "content": null
+    },
+    {
+      "source": {
+        "id": "news-com-au",
+        "name": "News.com.au"
+      },
+      "author": null,
+      "title": "Cricket greats fuming at ‘ridiculous’ scenes",
+      "description": "Welcome to news.com.au’s live coverage of Day 3 of the Second Test at the MCG.",
+      "url": "https://www.news.com.au/sport/cricket/australia-vs-pakistan-second-test-day-3-live-updates-from-mcg/news-story/45d20144f2c5956faac36229e3160503",
+      "urlToImage": "https://content.api.news/v3/images/bin/67efcbbb18f6565b63f1a2dad906a8ab",
+      "publishedAt": "2023-12-27T23:23:00Z",
+      "content": "Welcome to news.com.au’s live coverage of Day 3 of the Second Test at the MCG. \r\nPakistan resumes its innings on Thursday morning at 6/194, looking to push towards Australia’s first innings total of … [+5673 chars]"
+    },
+    {
+      "source": {
+        "id": "espn-cric-info",
+        "name": "ESPN Cric Info"
+      },
+      "author": null,
+      "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+      "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+      "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+      "publishedAt": "2020-04-27T11:41:47Z",
+      "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+    },
+    {
+      "source": {
+        "id": "espn-cric-info",
+        "name": "ESPN Cric Info"
+      },
+      "author": null,
+      "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+      "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+      "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+      "publishedAt": "2020-03-30T15:26:05Z",
+      "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+    }
+  ]
+}
